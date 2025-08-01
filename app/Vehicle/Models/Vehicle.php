488a663005654f641a839bd57dd8241e8889d5a5
SupianIDz <supianidz@gmail.com>
2025-08-02 01:51:15 +0800

    feat(vehicle): add custom `plate` accessor for formatting
    - Implement a custom `plate` accessor to format vehicle plate numbers.

--- app/Vehicle/Models/Vehicle.php
+++ app/Vehicle/Models/Vehicle.php
@@ -4,6 +4,7 @@ namespace App\Vehicle\Models;
 
 use App\Vehicle\Observers\VehicleObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,19 @@ class Vehicle extends Model
     protected $fillable = [
         'plate', 'brand', 'model',
     ];
+
+    /**
+     * @return Attribute
+     */
+    protected function plate() : Attribute
+    {
+        return
+            Attribute::make(static function ($value) {
+                if (preg_match('/^([A-Z]{1,2})(\d{1,4})([A-Z]{0,3})$/', $value, $m)) {
+                    return $m[1] . ' ' . $m[2] . ' ' . $m[3];
+                }
+
+                return $value;
+            });
+    }
 }
